ec8c043d4def9cf978d6f115c38d275db8cbafdf
Asan <amemetoff@yandex.ru>
2022-05-24 21:38:08 +0500

    add all models and migrations & add fix app.listen in app.js

---
+++ db/migrations/20220524160446-create-device.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Devices', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      img: {
+        type: Sequelize.STRING
+      },
+      new_device: {
+        type: Sequelize.BOOLEAN
+      },
+      used_device: {
+        type: Sequelize.BOOLEAN
+      },
+      type_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: {
+             tableName: 'Types',
+            },
+          key: 'id',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Devices');
+  }
+};
---
+++ db/models/device.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Device extends Model {
+    static associate({Type,Order}) {
+      this.belongsTo(Type, { foreignKey: 'type_id' });
+      this.belongsTo(Order, { foreignKey: 'device_id' });
+    }
+  }
+  Device.init({
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    img: DataTypes.STRING,
+    new_device: DataTypes.BOOLEAN,
+    used_device: DataTypes.BOOLEAN,
+    type_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Device',
+  });
+  return Device;
+};
+
+
