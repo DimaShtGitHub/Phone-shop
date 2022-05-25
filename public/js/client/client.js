e08fded0f871a61b324f393d887ae06e844ffe72
Taras Gavrilyuk <tarastim@gmail.com>
2022-05-25 15:02:00 +0300

    link to categories and home

---
+++ public/js/client/client.js
@@ -0,0 +1,13 @@
+const categ = document.querySelector('.categ')
+const navbar = document.querySelector('.menu-point')
+
+
+categ?.addEventListener('click', async (event) => {
+  const categoryId = event.target.id
+  console.log(categoryId)
+  window.location.href = `/device/${categoryId}`;
+})
+
+navbar?.addEventListener('click', async (event) => {
+  window.location.href = `/`;
+})
