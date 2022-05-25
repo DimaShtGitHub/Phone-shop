b3bc3955180ee5b7f601023657317a95c2165f33
Dima <schtanckevitch.dima@yandex.ru>
2022-05-25 14:42:21 +0300

    add footer

--- routes/deviceRouter.js
+++ routes/deviceRouter.js
@@ -3,12 +3,15 @@ const {Device, Type} = require('../db/models');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const headphones = await Device.findAll({where: {type_id: 1}});
-  const accessories = await Device.findAll({where: {type_id: 2}});
-  const phones = await Device.findAll({where: {type_id: 3}});
-  const laptops = await Device.findAll({where: {type_id: 4}});
-
-  res.render('', {headphones, accessories, phones, laptops});
+  const headphones = await Device.findAll({where: {type_id: 2}});
+  const accessories = await Device.findAll({where: {type_id: 4}});
+  const phones = await Device.findAll({where: {type_id: 1}, raw:true});
+  const laptops = await Device.findAll({where: {type_id: 3}});
+  console.log(headphones);
+  console.log(accessories);
+  console.log(phones);
+  console.log(laptops);
+  res.render('device', {phones});
 })
 
 router.get('/:id', async (req, res) => {
